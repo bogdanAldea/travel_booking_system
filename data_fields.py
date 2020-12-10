c3b098783ea60ffe2afb176875e157d4bd5d2986
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2020-12-10 15:36:13 +0200

    added pre-defined form fields

---
+++ data_fields.py
@@ -0,0 +1,17 @@
+class ClientFormFields:
+    RegistrationDate = "Data completarii"
+    FirstName = "Prenume"
+    LastName = "Nume"
+    CNP = "CNP"
+    PhoneNumber = "Numar de telefon"
+    Email = "E-mail"
+    Destinations = "Locatii dorite"
+    Dates = "Perioade dorite"
+    NumberOfPersons = "Numar de persoane"
+    AvailableAmount = "Suma disponibila"
+    OfferOption = "Oferta"
+    PaymentOption = "Plata"
+
+    fields_list = [RegistrationDate, LastName, FirstName, CNP,
+                   PhoneNumber, Email, Destinations, Dates,
+                   NumberOfPersons, AvailableAmount, OfferOption, PaymentOption]
