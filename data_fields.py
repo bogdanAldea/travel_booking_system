c73f6d63571fd6c2d848f9afe3a492ea90c9e199
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2020-12-16 19:53:16 +0200

    added new class of fields

--- data_fields.py
+++ data_fields.py
@@ -1,17 +1,35 @@
 class ClientFormFields:
-    RegistrationDate = "Data completarii"
-    FirstName = "Prenume"
-    LastName = "Nume"
+    RegistrationDate = "data_completare"
+    FirstName = "prenume"
+    LastName = "nume"
     CNP = "CNP"
-    PhoneNumber = "Numar de telefon"
-    Email = "E-mail"
-    Destinations = "Locatii dorite"
-    Dates = "Perioade dorite"
-    NumberOfPersons = "Numar de persoane"
-    AvailableAmount = "Suma disponibila"
-    OfferOption = "Oferta"
-    PaymentOption = "Plata"
+    PhoneNumber = "numar_telefon"
+    Email = "e_mail"
+    Destinations = "locatii_dorite"
+    Dates = "perioade_dorite"
+    StartDate = "data_start"
+    EndDate = "data_sfarsit"
+    NumberOfPersons = "numar_persoane"
+    AvailableAmount = "suma_disponibila"
+    OfferOption = "oferta"
+    PaymentOption = "plata"
 
     fields_list = [RegistrationDate, LastName, FirstName, CNP,
                    PhoneNumber, Email, Destinations, Dates,
                    NumberOfPersons, AvailableAmount, OfferOption, PaymentOption]
+
+
+class OfferFormFields:
+    ID = "ID"
+    Location = "Locatie"
+    Dates = "Perioade"
+    MinNumberOfPersons = "Grup min"
+    MaxNumberOfPersons = "Grup max"
+    HotelName = "Hotel"
+    HotelAddress = "Adresa Hotel"
+    Confort = "Confort"
+    Food = "Mese incluse"
+    Rooms = "Cazari disponibile"
+
+    fields_list = [ID, Location, Dates, MinNumberOfPersons, MaxNumberOfPersons,
+                   HotelName, HotelAddress, Confort, Food, Rooms]
